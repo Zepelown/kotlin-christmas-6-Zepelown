dcde18d973a7b42a2da2641e33d32a14a7c7df61
SeongWonYoon <dbswjdqhd12@gmail.com>
2023-11-14 01:31:34 +0900

    Test: "추가 메뉴 입력 테스트 코드"
    음료수만 입력, 메뉴 중복 입력 테스트 케이스 추가

--- src/test/kotlin/christmas/util/InputParserTest.kt
+++ src/test/kotlin/christmas/util/InputParserTest.kt
@@ -10,24 +10,29 @@ import org.junit.jupiter.params.provider.ValueSource
 class InputParserTest {
 
     @ParameterizedTest
-    @ValueSource(strings = ["양송이수프-1,아이스크림-2","해산물파스타-2,레드와인-1,초코케이크-1"])
-    fun `정상적인 값 입력`(input : String) {
-        assertDoesNotThrow { InputParser.parseOrder(input)  }
+    @ValueSource(strings = ["양송이수프-1,아이스크림-2", "해산물파스타-2,레드와인-1,초코케이크-1"])
+    fun `정상적인 값 입력`(input: String) {
+        assertDoesNotThrow { InputParser.parseOrder(input) }
     }
 
 
     @ParameterizedTest
-    @ValueSource(strings= [" ","asdfasdf","asdfqwer_123", "123", "`"])
-    fun `형식에 맞지 않는 메뉴 입력`(input: String){
+    @ValueSource(strings = [" ", "asdfasdf", "asdfqwer_123", "123", "`"])
+    fun `형식에 맞지 않는 메뉴 입력`(input: String) {
         assertThrows<IllegalArgumentException> { InputParser.parseOrder(input) }
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["타파스-1,타파스-2"])
-    fun `메뉴 중복 입력`(input: String){
+    @ValueSource(strings = ["타파스-1,타파스-2", "아이스크림-1,아이스크림-2","타파스-1,양동이수프-2,타파스-3"])
+    fun `메뉴 중복 입력`(input: String) {
         assertThrows<IllegalArgumentException> { InputParser.parseOrder(input) }
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = ["제로콜라-1","제로콜라-2,레드와인-3"])
+    fun `음료수만 입력`(input: String) {
+        assertThrows<IllegalArgumentException> { InputParser.parseOrder(input) }
+    }
 
 
 }
